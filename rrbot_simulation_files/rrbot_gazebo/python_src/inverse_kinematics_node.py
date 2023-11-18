3b85c6202f2e77070ccf90a58476c4e29391fca4
joshfarina <jfarino2112@gmail.com>
2023-11-18 17:26:42 -0600

    added ik equations, changed after new corrected derivations

--- rrbot_simulation_files/rrbot_gazebo/python_src/inverse_kinematics_node.py
+++ rrbot_simulation_files/rrbot_gazebo/python_src/inverse_kinematics_node.py
@@ -11,6 +11,12 @@ L1 = 1  # [m]
 L2 = 2  # [m]
 L3 = 3  # [m]
 
+# Test Coordinates:
+x_c = 2
+y_c = 3
+z_c = 1
+test_end_effector_coordinates = [x_c, y_c, z_c]
+
 class ComputeInverseKinematics(Node):
 
     # Computes the inverse kinematics for a 3-DOF SCARA robotic arm
@@ -26,20 +32,27 @@ class ComputeInverseKinematics(Node):
 
         self.get_logger().info(f"Initializing {self.get_name()}...")
 
-    def listener_callback(self, msg: JointState) -> None:
-
-        #End effector position take from Gazebo? or FK Output?
-        # assert len(msg.position) == 3, "Needs to accept 3 joint angles only."
-
-        self.get_logger().info(f"I heard: {msg}")
-        self.get_logger().info(f"Resulting pose, given q1 q2 q3:\n {end_effector_pose}")
-
-    def compute_inverse_kinematics(link_lengths: list) -> np.ndarray:
+    def compute_inverse_kinematics_callback(test_end_effector_coordinates, link_lengths: list) -> np.ndarray:
 
         l1, l2, l3 = link_lengths
+        # pseudo for now until ROS coordinate array setup
+
+        r = np.sqrt(test_end_effector_coordinates[0]**2 + test_end_effector_coordinates[1]**2)
+        # CHECK IF LINK LENGTHS ARE PROPERLY LABELED AND USED IN GAZEBO, LABELED AS a IN DERIVATION (setting as l2 for now)
+        cos_theta2 = (r**2 - l2**2 - l3**2)/(2*l3)
+        sin_theta2 = np.sqrt(1-cos_theta2)
+        joint2_angle = np.arctan2(sin_theta2, cos_theta2)
+
+        find_theta1_phi = np.arctan2(test_end_effector_coordinates[1], test_end_effector_coordinates[0]) #replace with coordinate array from ROS or service request input
+        find_theta1_beta = np.arctan2((l3*sin_theta2)/(l2 + l3*cos_theta2)) #double-check correct link length assignment
+        joint1_angle = find_theta1_phi - find_theta1_beta
+
+        prismatic3_position = l1 - test_end_effector_coordinates[2] #check correct link assignment
+
+        computed_joint_angles_positions = [joint1_angle, joint2_angle, prismatic3_position]
 
 
-        return computed_joint_angles
+        return computed_joint_angles_positions
 
 
 def main(args=None):
